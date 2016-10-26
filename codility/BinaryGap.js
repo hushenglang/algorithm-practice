2c0dec1a4c2d95a6ae1359f5a207931cb0e6e1a9
Joe Hu <joehu@ecargo.com>
2016-10-26 17:20:02 +0800

    add codility BinaryGap practice.

---
+++ codility/BinaryGap.js
@@ -0,0 +1,24 @@
+/**
+ * Created by joehu on 26/10/2016.
+ */
+
+function solution(N) {
+    // write your code in JavaScript (Node.js 6.4.0)
+    var zerosList = Number(N).toString(2).split("1");
+    var zerosLengthList = zerosList.map(s=>s.length);
+    var max = 0;
+    while(true){
+        max = Math.max(...zerosLengthList);
+        var index = zerosLengthList.indexOf(max)
+        if(index==zerosLengthList.length-1){
+            zerosLengthList[index]=0;
+            max = 0;
+        }else{
+            break;
+        }
+    }
+    return max;
+}
+
+console.log(solution(6));
+console.log(solution(148));
